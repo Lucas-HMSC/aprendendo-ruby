355c81eaab5a23f7f223335850ee08d84ace6695
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-11-18 20:46:04 -0300

    Create exer3.rb

---
+++ exer3.rb
@@ -0,0 +1,12 @@
+# Author: Lucas-HMSC
+
+# Método strip retorna uma String sem os espaços iniciais e finais, mas não altera a varíavel
+autor = "   Lucas-HMSC   "
+
+puts autor.strip
+puts autor 
+
+# Para alterar, deve-se utilizar o !
+puts autor.strip!
+puts autor
+
