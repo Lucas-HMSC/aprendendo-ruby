3c92408a0db8937b33b9ca865b06d3c9d251f4eb
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-11-19 21:06:15 -0300

    Create exer16.rb

---
+++ exer16.rb
@@ -0,0 +1,15 @@
+# Author: Lucas-HMSC
+
+rede_social = "GitHub"
+
+case rede_social
+when "GitHub"
+    puts "Lucas-HMSC"
+when "Facebook"
+    puts "Lucas Henrique"
+when "LinkedIn"
+    puts "dev-lucas-carvalho"
+else
+    puts "Escolha uma rede social"
+end
+
