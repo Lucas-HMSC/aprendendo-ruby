49e69c6abdc7e990fb4a66d3252b160692a2f443
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-11-26 19:05:18 -0300

    Create exer22.rb

---
+++ exer22.rb
@@ -0,0 +1,9 @@
+# Author: Lucas-HMSC
+# Tipos de números
+
+puts 50.class # Inteiro
+puts 50.5.class # Float
+puts 0b1010 # Escrita binaria, retorna decimal - '10' 
+puts 0o45 # Escrita octal, retorna decimal '37'
+puts 0xFF # Escrita hexadecimal, retorna decimal '255'
+
