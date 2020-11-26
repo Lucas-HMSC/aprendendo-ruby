a4f5bde8f970d6a54b5db4ca3f97cd5d9370c219
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-11-26 19:15:46 -0300

    Create exer24.rb

---
+++ exer24.rb
@@ -0,0 +1,11 @@
+# Author: Lucas-HMSC
+# Operadores de Strings
+
+nome = 'Lucas Carvalho'
+
+puts nome.downcase # lucas carvalho
+puts nome.upcase # LUCAS CARVALHO
+nome["Carvalho"] = "Henrique"
+puts nome # Lucas Henrique
+puts nome.capitalize # Lucas henrique
+puts nome.length # 14
