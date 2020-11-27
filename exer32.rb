416ba576dce5517b25e3721e3f775ef1f451e52a
lucas-hmsc <lucashms.carvalho@gmail.com>
2020-11-27 19:23:56 -0300

    Create exer32.rb

---
+++ exer32.rb
@@ -0,0 +1,15 @@
+# Author: Lucas-HMSC
+# Constantes
+
+NOME = "Lucas Carvalho" 
+puts NOME # Lucas Carvalho
+
+NOME = "Lucas Henrique" 
+puts NOME # Lucas Henrique
+# Ruby permite a alteração de Constantes
+
+class Dados
+    DADOS_AUTHOR = ["Lucas Carvalho", "Lucas-HMSC"]
+end
+
+puts Dados::DADOS_AUTHOR # ["Lucas Carvalho", "Lucas-HMSC"]
